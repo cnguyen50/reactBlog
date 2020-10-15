17e1f93e9e219d08a95f20d9722c60658b9408e0
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-10-14 18:31:45 -0700

    updating blog post on render

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,14 +3,17 @@ import React, { useState } from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  const [blogPost, setBlogPosts] = useState([]);
+  const [blogPosts, setBlogPosts] = useState([]);
 
   const addBlogPost = () => {
-    setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}` }])
+    setBlogPosts([
+      ...blogPosts, 
+      { title: `Blog Post #${blogPosts.length + 1}` }
+    ])
   }
   
   return (
-    <BlogContext.Provider value={{ data: blogPosts, addBlogPost: addBlogPost }}>
+    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
       {children}
     </BlogContext.Provider>
   )
