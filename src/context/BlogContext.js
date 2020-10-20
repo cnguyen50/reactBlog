cdb1b5f615c7af37b362a3e686451a5887efc91f
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-10-20 15:37:55 -0700

    gave blog post random id

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -6,7 +6,7 @@ const BlogContext = React.createContext();
 const blogReducer = (state, action) => {
   switch (action.type) {
     case 'add_blogpost':
-      return [...state, { title: `Blog Post #${state.length + 1}` }]
+      return [...state, { id: Math.floor(Math.random() * 99999), title: `Blog Post #${state.length + 1}` }]
     default:
       return state;
   }
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -16,7 +16,7 @@ const IndexScreen = () => {
         renderItem={({ item }) => {
           return (
             <View style={styles.row}>
-              <Text style={styles.title}>{item.title}</Text>
+              <Text style={styles.title}>{item.title} - {item.id}</Text>
               <Feather style={styles.icon} name="trash" />
             </View>
           )
