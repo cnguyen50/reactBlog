0918c4e35b57a5d773c44c1c3c418b3a906af839
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-10-21 18:38:13 -0700

    added edit icon and boidlerplate for edit screen

---
+++ src/screens/EditScreen.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, Text, StyleSheet} from 'react-native'
+
+const EditScreen = () => {
+
+  return (
+    <View>
+      <Text>Edit Screen</Text>>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  
+})
+
+export default EditScreen;
