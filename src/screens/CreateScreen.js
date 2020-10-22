0f193850baa1165959dee8c0350f8bd91e48d616
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-10-21 17:54:44 -0700

    boiler plate for creae screen

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { Context } from '../context/BlogContext'
+import { State } from 'react-native-gesture-handler'
+
+const CreateScreen = ({ navigation }) => {
+
+  return (
+    <View>
+      <Text>Create</Text>
+    </View>
+  )
+}
+
+export default CreateScreen;
