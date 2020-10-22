37a9e0149eb8024a8d8ad8744325f720a6126e0d
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-10-21 19:03:25 -0700

    revied create and edit components

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -4,49 +4,9 @@ import { Context } from '../context/BlogContext'
 import { State } from 'react-native-gesture-handler'
 
 const CreateScreen = ({ navigation }) => {
-  const [ title, setTitle ] = useState('');
-  const [ content, setContent ] = useState('')
   const { addBlogPost } = useContext(Context)
 
-  return (
-    <View>
-      <Text style={styles.label}>Enter Title</Text>
-      <TextInput 
-        style={styles.input} 
-        value={title} 
-        onChangeText={text => setTitle(text)} />
-      <Text style={styles.label}>Enter Content:</Text>
-      <TextInput 
-        style={styles.input}
-        value={content} 
-        onChangeText={text => setContent(text)} />
-
-      <Button 
-        title="Add Blog Post"
-        onPress={() => {
-          addBlogPost(title, content, () => {
-            navigation.navigate('Index')
-          })
-        }}
-      />
-    </View>
-  )
 }
 
-const styles = StyleSheet.create({
-  input: {
-    fontSize: 18,
-    borderColor: 'black',
-    borderWidth: 1,
-    marginBottom: 15,
-    padding: 5,
-    margin: 5
-  },
-  label: {
-    fontSize: 20,
-    marginBottom: 5,
-    marginLeft: 5
-  }
-})
 
 export default CreateScreen;
