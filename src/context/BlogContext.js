6dbc26f65d406f79081a0414e23a80d452a8060f
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-10-23 13:27:54 -0700

    async axios post req for delete post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -45,7 +45,8 @@ const addBlogPost = (dispatch) => {
 };
 
 const deleteBlogPost = (dispatch) => {
-  return (id) => {
+  return async (id) => {
+    await jsonServer.delete(`/blogposts/${id}`)
     dispatch({ type: 'delete_blogpost', payload: id })
   }
 };
